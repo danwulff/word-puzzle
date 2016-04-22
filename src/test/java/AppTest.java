809f2c6dab9d57b9448c8a29e0b88a59672d1023
Daniel Wulff <danielwulff.ee@gmail.com>
2016-04-22 15:06:43 -0700

    created interface tests for webpages

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,45 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test //basic index test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Enter a sentence");
+  }
+  @Test //basic result page test
+  public void resultTest() {
+    goTo("http://localhost:4567/result?inputSentence=");
+    assertThat(pageSource()).contains("Can you guess");
+  }
+  @Test //test a Sentence
+  public void sentenceTest() {
+    goTo("http://localhost:4567/result?inputSentence=Ain%27t+no+rest+for+the+wicked");
+    assertThat(pageSource()).contains("--n't n- r-st f-r th- w-ck-d");
+  }
+  @Test //test upper case alphabet
+  public void upperAlphabetTest() {
+    goTo("http://localhost:4567/result?inputSentence=ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    assertThat(pageSource()).contains("-BCD-FGH-JKLMN-PQRST-VWXYZ");
+  }
+  @Test //test lower case alphabet
+  public void lowerAlphabetTest() {
+    goTo("http://localhost:4567/result?inputSentence=abcdefghijklmnopqrstuvwxyz");
+    assertThat(pageSource()).contains("-bcd-fgh-jklmn-pqrst-vwxyz");
+  }
+}
